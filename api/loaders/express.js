16c7d45b7426bdf2d36f6627dbdff5e2ad42b4e4
p4w31 \!d21k0w541 <iddziku@gmail.com>
2024-06-03 19:45:58 +0200

    Add simple POST request

--- api/loaders/express.js
+++ api/loaders/express.js
@@ -1,9 +1,38 @@
+import express from 'express';
+
+/**
+ * @param { import("express").Application } app
+ * @param { import("@prisma/client").PrismaClient } prisma
+ */
 export default (app, prisma) => {
+    app.use(express.json())
+
     app.get('/users', async function (req, res) {
         const users = await prisma.user.findMany();
         
-        return res.send({
+        return res.status(200).send({
             data: users
+        });
+    })
+
+    app.post('/users', async function (req, res) {
+        const { body: { email, name } } = req;
+
+        if (!email || !name || typeof email !== 'string' || typeof name !== 'string') {
+            return res.status(400).send({
+                error: 'Validation error!'
+            })
+        }
+
+        const user = await prisma.user.create({
+            data: {
+              name,
+              email,
+            },
+          })
+
+        return res.status(201).send({
+            data: user
         });
     })
 }
