0eddaa77727487188f010d8cd6dd0deaf36e54b3
p4w31 \!d21k0w541 <iddziku@gmail.com>
2024-06-05 17:32:05 +0200

    Prepare simple app

---
+++ client/src/components/Users.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Your users:</h1>
+
+        <div v-if="error" class="text-error">
+            An error occurred when trying to fetch data!
+
+            <div class="mt-10">
+                <strong>Reason:</strong>
+
+                <blockquote>
+                    {{ error }}
+                </blockquote>
+            </div>
+        </div>
+
+        <div v-else>
+            <div v-for="(user, index) in value" :key="index">
+                {{ user.name }} - {{ user.email }}
+            </div>
+        </div>
+
+        <div class="mt-10">
+            <slot name="actions" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Array,
+            required: true
+        },
+
+        error: {
+            type: String,
+            required: true
+        }
+    },
+};
+</script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,58 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-view">
+    <div class="block">
+      <Users :value="usersStore.USERS" :error="getAllError">
+        <template #actions>
+          <button :disabled="isLoading" @click="fetchUsers">Try again</button>
+        </template>
+      </Users>
+    </div>
+
+    <div class="block">
+      <AddUserForm />
+    </div>
+  </div>
 </template>
+
+<script>
+import { useUsersStore } from "@/stores/users";
+
+import Users from "@/components/Users.vue";
+import AddUserForm from "@/components/AddUserForm.vue";
+
+export default {
+  components: {
+    Users,
+    AddUserForm
+  },
+
+  data() {
+    const usersStore = useUsersStore();
+
+    return {
+      usersStore,
+      getAllError: "",
+      isLoading: false,
+    };
+  },
+
+  methods: {
+    async fetchUsers() {
+      this.getAllError = '';
+      this.isLoading = true;
+
+      try {
+        await this.usersStore.getAll();
+      } catch (error) {
+        this.getAllError = error.message;
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  },
+
+  created() {
+    this.fetchUsers();
+  }
+};
+</script>
