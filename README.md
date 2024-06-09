61ccdafc6ecf9c2d7f9afb119c981266fd84463a
p4w31 !d21k0w541 <iddziku@gmail.com>
2024-06-10 00:49:19 +0200

    Feature/automated quick setup (#11)
    * Initialize quick start script
    
    * Add quick start logic
    
    * Modify env file
    
    * Add temporary fix for API_URL
    
    * Modify example inventory file
    
    * update README
    
    * update README
    
    * Update notes in script
    
    * Add option to re-run script from specific step
    
    * up README
    
    * Specify strict mode function name
    
    * Return error if pod name is missing

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-### Stack
+## Stack
 
-- Node.js
-- Ansible
 - Docker
+- Ansible
 - Terraform
+- Node.js, Vue, PostgreSQL
 - K8S cluster ([setup with Docker Desktop](https://docs.docker.com/desktop/kubernetes/)), kubectl
 
-### Preview
+## Preview
 
 ```
           +-----------------+
@@ -14,7 +14,7 @@
           |    CLUSTER      |
           |                 |
 +-----+       +--------+    |
-| WWW | <---> | CLIENT |    |
+| WEB | <---> | CLIENT |    |
 +-----+       +--------+    |
           |       ^         |
           |       |         |
@@ -26,68 +26,88 @@
           +-----------------+
 ```
 
-### Quick start
+## Running project
+
+> [!WARNING]
+> Before taking any action:
 
 1. Check if `ssh` is installed (`ssh -V`)
+2. Prepare VM/machine for database setup via Ansible. It should have:
 
-2. Generate key named `id_cass`
+   - `openssh-server` installed
+   - configured ssh connection with your machine (to change ssh key name view [ansible.cfg](./database/ansible.cfg))
+
+   <details>
+   <summary>How to configure SSH connection?</summary>
+      
+   1. Generate key named `id_cass`
 
    ```sh
-   ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_cass
+   sh-keygen -t rsa -b 4096 -f ~/.ssh/id_cass
    ```
 
-3. Go to `/database`
+   2. Paste public key (`~/.ssh/id_cass.pub`) content to `~/.ssh/authorized_keys` on VM/machine
 
-   1. Prepare Debian or other Unix OS based VM/machine and define `inventory`
+   </details>
+
+> [!WARNING]
+> Ansible playbook uses `su root` to access sudoer user (Debian OS). For different command or username, please modify [setup.yaml](./database/setup.yaml) ([see become-directives](https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_privilege_escalation.html#become-directives))
+
+### Quick start 🔥
+
+1. Get VM/machine host (`ip a`) and define `inventory`:
+
+   ```sh
+   [postgres]
+   db1 ansible_host=192.168.0.7
+   ```
+
+   > Use `ansible postgres -m ping` to test if it's reachable
+
+2. Execute `quick-start.sh` script from root dir.
+
+### Manual start ⚙️
+
+1. Go to `/database`
+
+   1. Get VM/machine host (`ip a`) and define `inventory`:
 
       ```sh
       [postgres]
       db1 ansible_host=192.168.0.7
       ```
 
-   2. Make sure that `db1` has `openssh-server` installed
-
-      ```sh
-      sudo apt install openssh-server
-      ```
-
-   3. Add public key (`~/.ssh/id_cass.pub`) to `~/.ssh/authorized_keys` file in `db1`
-
-   4. Test connection
+   2. Test connection
 
       ```sh
       ansible postgres -m ping
       ```
 
-   5. Execute [setup.yaml](./database/setup.yaml) playbook for PostgreSQL role
+   3. Execute [setup.yaml](./database/setup.yaml) playbook for PostgreSQL role
 
       ```sh
       ansible-playbook setup.yaml --extra-vars "role=postgres" -K
       ```
 
-      > [!WARNING]
-      > Playbook uses `su root` to access sudoer user. For different command or username, adjust [setup.yaml](./database/setup.yaml) ([see become-directives](https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_privilege_escalation.html#become-directives))
+2. Create `.env` files (x3) based on `.env.example`.
 
-4. Create `.env` files (x3) based on `.env.example`.
+3. Modify `DATABASE_URL` and `SHADOW_DATABASE_URL` in `/api/.env` by replacing address (`127.0.0.1`) to the one that you've provided in `inventory`.
 
-5. Modify `DATABASE_URL` and `SHADOW_DATABASE_URL` in `/api/.env` by replacing address (`127.0.0.1`) to the one that you've provided in `inventory`.
-
-6. Go to root dir of project and build `client` and `api` images.
+4. Go to root dir of project and build `client` and `api` images.
 
    ```
    npm run dc-build
    ```
 
-7. Go to [/infrastructure-as-code](./infrastructure-as-code/) and call:
+5. Go to [/infrastructure-as-code](./infrastructure-as-code/) and call:
 
    ```sh
    terraform apply
    ```
 
-   > [!NOTE]
    > If creating client service fails and you don't want to wait until K8S tries to resurrect it again, re-run it manually after few seconds
 
-8. Run migrations by accessing `api`:
+6. Run migrations by accessing `api`:
 
    ```sh
    # kubectl get pod
@@ -96,6 +116,6 @@
    npm run db:migrate:dev
    ```
 
-9. Access app at http://localhost:31000 or http://127.0.0.1:31000
+7. Access app at http://localhost:31000 or http://127.0.0.1:31000
 
-10. Destroy stack using `terraform destroy`
+8. Destroy stack using `terraform destroy`
---
+++ quick-start.sh
@@ -0,0 +1,267 @@
+#!/bin/bash
+
+COLOR_OFF='\033[0m'
+COLOR_RED='\033[0;91m'
+COLOR_CYAN='\033[0;96m'
+COLOR_GREEN='\033[0;92m'
+COLOR_YELLOW='\033[0;93m'
+
+current_step=1
+database_host=''
+database_provider='postgres'
+run_from_step=''
+
+run_in_strict_mode_without_pipefall() {
+    set -T # inherit DEBUG and RETURN trap for functions
+    set -C # prevent file overwrite by > &> <>
+    set -E # inherit -e
+    set -e # exit immediately on errors
+    set -u # exit on not assigned variables
+}
+
+load_args() {
+    while [ $# -gt 0 ]; do
+        case "$1" in
+        --from-step=*)
+            run_from_step="${1#*=}"
+            ;;
+        *)
+            log_error "Invalid argument passed to script!"
+            ;;
+        esac
+        shift
+    done
+}
+
+move_to_script_directory_if_needed() {
+    local dirname=$(dirname $0)
+
+    if [ "${dirname}" != "." ]; then
+        cd $dirname
+    fi
+}
+
+precheck() {
+    # &>/dev/null - suppress stdout and stderr
+    if ! command ansible --version &>/dev/null; then
+        log_error "Ansible not found!"
+    fi
+
+    if ! command terraform --version &>/dev/null; then
+        log_error "Terraform not found!"
+    fi
+
+    if ! command docker --version &>/dev/null; then
+        log_error "Docker not found!"
+    fi
+
+    if ! command kubectl version &>/dev/null; then
+        log_error "kubectl not found!"
+    fi
+
+    is_file_available "./database/inventory"
+}
+
+extract_database_host_from_inventory() {
+    log_note "Attempting to extract [${database_provider}] database host from inventory.."
+
+    database_host=$(grep -A2 "\[${database_provider}\]" ./database/inventory | grep -o -P '(?<=ansible_host=).*(?=( |$))')
+
+    if [ -z "${database_host}" ]; then
+        log_error "Extraction failed! Make sure that inventory includes [${database_provider}] entry with ansible_host definition below it!"
+    fi
+
+    sleep 0.2
+
+    log_note "Host: ${database_host}"
+}
+
+log() {
+    echo -e "$1[$2]: $3${COLOR_OFF}"
+}
+
+log_warning() {
+    log $COLOR_YELLOW "WARNING" "${1}"
+}
+
+log_error() {
+    log $COLOR_RED "ERROR" "${1}"
+
+    exit 1
+}
+
+log_success() {
+    log $COLOR_GREEN "SUCCESS" "${1}"
+}
+
+log_note() {
+    log $COLOR_CYAN "NOTE" "${1}"
+}
+
+execute_step() {
+    local is_skipped=1
+    local text="Step ${current_step}: $1"
+
+    if [ ! -z "${run_from_step}" ] && [ $current_step -lt $run_from_step ]; then
+        is_skipped=0
+        text+=" ${COLOR_YELLOW}(skipped)${COLOR_OFF}"
+    fi
+
+    log_note "${text}"
+
+    if [ $is_skipped -eq 1 ]; then
+        $2
+    fi
+
+    ((current_step++))
+}
+
+is_file_available() {
+    if ! test -f "$1"; then
+        log_error "File ${1} does not exist but is required to run script!"
+
+        exit 1
+    fi
+}
+
+is_action_confirmed() {
+    local text=$1
+
+    read -p "$(echo -e $text $COLOR_YELLOW"(y/n)"$COLOR_OFF) " choice
+
+    case "$choice" in
+    y | Y) return 0 ;;
+    n | N) return 1 ;;
+    *) is_action_confirmed "${text}" ;;
+    esac
+}
+
+require_confirmation() {
+    local text=$1
+
+    read -p "$(echo -e $text $COLOR_YELLOW"(y/n)"$COLOR_OFF) " choice
+
+    case "$choice" in
+    y) ;;
+    n) ;;
+    *) is_action_confirmed "${text}" ;;
+    esac
+
+    if [ "${choice}" != 'y' ]; then
+        log_note "Exiting..."
+
+        exit 0
+    fi
+}
+
+step_connection_test() {
+    local result=$(bash -c "ansible "${database_provider}" -m ping")
+
+    if grep -qi "unreachable" <<<"$result"; then
+        log_error "Ping of [${database_provider}] includes unreachable host(s)! Check if specified host(s) have openssh-server installed and include public key of id_cass in ~/.ssh/authorized_keys file"
+    fi
+
+    log_success "Connection OK"
+}
+
+step_database_setup() {
+    local result=$(ansible-playbook setup.yaml --extra-vars "role=${database_provider}" -K 2>&1 | tee /dev/tty)
+
+    if grep -qE "(unreachable|failed)=[1-9]+[0-9]{0,}" <<<"$result"; then
+        log_error "Running playbook failed!"
+    fi
+}
+
+step_init_envs() {
+    local example_env_filename='.env.example'
+    local env_filename='.env'
+
+    cp ${example_env_filename} ${env_filename}
+    cp ./api/${example_env_filename} ./api/${env_filename}
+    cp ./client/${example_env_filename} ./client/${env_filename}
+}
+
+step_adjust_database_connection_url() {
+    sed -i "s/REPLACE_WITH_VALID_HOST/${database_host}/g" ./api/.env
+}
+
+step_build_images() {
+    local result=$(docker compose build 2>&1 | tee /dev/tty)
+
+    if grep -iq "error" <<<"$result"; then
+        log_error "Failed to build Docker images!"
+    fi
+}
+
+step_apply_infrastructure() {
+    local result=$(terraform apply -auto-approve 2>&1 | tee /dev/tty)
+
+    if ! grep -iq "Apply complete!" <<<"$result"; then
+        log_error "Failed to apply infrastructure!"
+    fi
+}
+
+step_migrate_database() {
+    local pod_name=$(kubectl get pods -o=name | grep -o "api-.*" 2>&1)
+
+    if [ -z $pod_name ]; then
+        log_error "Failed to get api pod name!"
+    fi
+
+    local result=$(kubectl exec -it ${pod_name} -- bash -c "npm run db:migrate:dev" 2>&1 | tee /dev/tty)
+
+    if grep -iq "error" <<<"$result"; then
+        log_error "Failed to perform database migration!"
+    fi
+}
+
+# ---------------------------------------------------------------------------------------
+
+run_in_strict_mode_without_pipefall
+
+move_to_script_directory_if_needed
+
+precheck
+
+load_args "$@"
+
+if [ -z $run_from_step ]; then
+    log_note "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧"
+
+    log_note "This script performs quick start of 'simple-stack-playground' infrastructure in K8S cluster using Ansible and Terraform on preconfigured data. It does not have rollback feature. In case of any error, try to re-run from particular step."
+
+    log_warning "Script does not have rollback feature. In case of any error, try to re-run from particular step, e.g. 
+    ./quick-start.sh --from-step=4
+    "
+
+    require_confirmation "Do you want to run it?"
+fi
+
+# @TMP hardcoded because currently no other provider is possible to use for data center
+extract_database_host_from_inventory "postgres"
+
+require_confirmation "Is host correct?"
+
+cd ./database
+
+execute_step "Testing connection.." "step_connection_test"
+
+execute_step "Preparing ${database_provider}.." "step_database_setup"
+
+cd ..
+
+execute_step "Creating env files.." "step_init_envs"
+
+execute_step "Adjusting DATABASE_URL/SHADOW_DATABASE_URL.." "step_adjust_database_connection_url"
+
+execute_step "Building Docker images.." "step_build_images"
+
+cd ./infrastructure-as-code
+
+execute_step "Applying infrastructure.." "step_apply_infrastructure"
+
+execute_step "Migrating database.." "step_migrate_database"
+
+log_success "Script completed. Access app at http://localhost:31000 or http://127.0.0.1:31000"
+
+log_note "To destroy infrastructure, use 'terraform destroy'"
